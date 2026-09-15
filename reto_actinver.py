1bd7b00c7eccca0fee5a91b1fee9225e8bacaeb6
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-09-15 13:40:51 -0600

    Correct Ex-Dividend Date formatting in reto_actinver.py
    Fix formatting issue with Ex-Dividend Date retrieval.

--- reto_actinver.py
+++ reto_actinver.py
@@ -89,7 +89,6 @@ def obtener_fundamentales(ticker):
             "Return on Equity": info.get("returnOnEquity"),
             "Dividend Yield": info.get("dividendYield"),
             "1Y Target Estimate": info.get("targetMeanPrice"),
-            "Ex-Dividend Date": datetime.fromtimestamp(info.get("exDividendDate")).strftime("%d/%m/%Y")
                     
         }
     except Exception:
